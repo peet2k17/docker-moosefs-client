b34fa0498f8e8719234f248a3a80ef810d77e815
peet2k17 <35061089+peet2k17@users.noreply.github.com>
2019-05-21 23:37:54 +0200

    Update start-client.sh

--- start-client.sh
+++ start-client.sh
@@ -5,8 +5,8 @@ ping -c 10 mfsmaster
 
 # unmount fuse
 fusermount -uz "/mnt/moosefs"
-#rm -r /mnt/moosefs
-#mkdir -p /mnt/moosefs
+rm -r /mnt/moosefs
+mkdir -p /mnt/moosefs
 
 # mount mfs
 mfsmount /mnt/moosefs -H mfsmaster
